661ca80a0f3b878c3f1d7811dbb51fe91c32c71d
hydratim <timothy.armstrong@duxdigital.co.uk>
2019-09-21 23:06:07 +0100

    minor modifications

--- pyOTDR/__init__.py
+++ pyOTDR/__init__.py
@@ -9,9 +9,9 @@ For using pyOTDR in a module, see the code in pyOTDR/main.py.  The two main func
 
 """
 
-from .read import sorparse
+from .read import sorparse, sorstream
 from .dump import tofile
 from .main import main
 
-__version__ = "1.0.0.c1"
+__version__ = "1.0.0.c2"
 
